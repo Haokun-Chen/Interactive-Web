3ad4e471d0919466d08c275fd0c520d7f094c946
Haokun Chen <chenhk@Haokuns-MacBook-Air.local>
2017-01-05 19:47:55 +0800

    Add JS files

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+//Twitter
+!function(d, s, id){
+    var js,fjs=d.getElementsByTagName(s)[0],
+    p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)){
+        js=d.createElement(s);js.id=id;
+        js.src=p+'//platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js,fjs);}
+    }(document, 'script', 'twitter-wjs');
+
+//Facebook
+!function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+        fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk');
+
+
+$(document).ready(function(){
+    // Smooth scrolling
+    var $root = $('html, body');
+    $('.navbar-nav a').click(function() {
+        var href = $.attr(this, 'href');
+        $root.animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
+        return false;
+    });
+
+    //Change color
+    alert("Use JS to chagne background-color in message-box!");
+    $('.message-box').css("background-color","red");
+});
+
+ // Stellar
+$.stellar();
+
+  // Tooltips
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+// button clicked function
+$('button').on('click',function(){
+    var comment = $('.message-box').val();
+    // get the string from textarea
+    console.log(comment);
+    $('.visible-comment').html(comment);
+    $('.message-box').hide();
+    return false;
+});
+
+
